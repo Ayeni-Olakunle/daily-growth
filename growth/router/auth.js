723cc2aa7f1017561db0b41fd623643131a6bdf7
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-08-08 15:23:45 +0100

    update

--- growth/router/auth.js
+++ growth/router/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const signup = express.Router();
 const { getAllUser } = require("../controllers/auth/signup");
 
-signup.route("/").get(getAllUser)
+signup.route("/").get(getAllUser);
 
 module.exports = signup;
 
